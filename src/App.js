838d2a983735e378d8558625a63fc04ac6aaa495
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-10-10 00:00:39 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+
+
+const CheckboxExample = () => {
+  const [checked, setChecked] = useState(false)
+  const [checked2, setChecked2] = useState(false)
+
+  return (
+    <div>
+      <FormControlLabel control={<Checkbox checked={checked} onChange={(e) => setChecked(e.target.checked)} inputProps={{ 'aria-label' : 'secondary checkbox' }} />} label="Checkbox 1" /> 
+      <FormControlLabel control={<Checkbox checked2={checked2} onChange={(e) => setChecked2(e.target.checked2)} inputProps={{ 'aria-label' : 'secondary checkbox' }} />} label="Checkbox 2" />  
+    </div>
+  )
+}
 
 const App = () =>  {
   return (
     <div className="App">
       <header className="App-header">
-      <Button size="large" style={{ fontSize: 30 }} variant="contained" color="secondary">
-        Hello World
-      </Button>
+        <TextField label="Enter Name" type="name" variant="outlined" color="secondary" style={{ paddingBottom : "40px"}}/>
+        <CheckboxExample />
+      <ButtonGroup >
+        <Button startIcon={<SaveIcon/>} size="large" variant="contained" color="primary">
+          Save
+        </Button>
+        <Button startIcon={<DeleteIcon/>} size="large" variant="contained" color="secondary">
+          Delete
+        </Button>
+      </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
